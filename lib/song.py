697da72f4a92e80c9b92c827ec68d00e6fb57b4e
Leigh Dotey <leighdotey@gmail.com>
2024-08-20 16:58:19 -0400

    completed lab

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,49 @@
 class Song:
+    count = 0
+    genres = []
+    artists = []
+    genre_count = {}
+    artist_count = {}
+
+    def __init__(self, name, artist, genre):
+        self.add_song_to_count()
+        self.add_to_genres(genre)
+        self.add_to_artists(artist)
+        self.add_to_genre_count(genre)
+        self.add_to_artist_count(artist)
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+
+    @classmethod
+    def add_song_to_count(cls):
+        cls.count += 1
+
+    @classmethod
+    def add_to_genres(cls, genre):
+        if genre not in cls.genres:
+            cls.genres.append(genre)
+
+    @classmethod
+    def add_to_artists(cls, artist):
+        if artist not in cls.artists:
+            cls.artists.append(artist)
+
+    @classmethod
+    def add_to_genre_count(cls, genre):
+        #check if this specific genre is already a key
+        #in the genre_count dictionary{}
+        #if it's not there then add that genre and initialize the count to 1
+        if genre not in cls.genre_count:
+            cls.genre_count[genre] = 1
+        #if that genre is already in the dict then increment the value by 1
+        else:
+            cls.genre_count[genre] += 1
+
+    @classmethod
+    def add_to_artist_count(cls, artist):
+        if artist not in cls.artist_count:
+            cls.artist_count[artist] = 1
+        else:
+            cls.artist_count[artist] += 1
     pass
